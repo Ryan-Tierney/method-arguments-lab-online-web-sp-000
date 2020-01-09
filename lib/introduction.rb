c84d732900709d51cd94f038064831e4bb110613
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-09 00:07:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,7 @@
 def introduction(name)
-  puts "Hi my name is #{name} and I am learning to program in Ember."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction(name, languague)
+  puts "Hi, my name is #{name}, and I am learning to be a #{languague} programmer."
 end
